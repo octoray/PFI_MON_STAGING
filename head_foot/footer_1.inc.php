03a8c77cb0b5ad303bbe98c66030efc5fc60692e
octoray <david.wintour@gmail.com>
2015-01-05 12:07:27 +0000

    add more

---
+++ head_foot/footer_1.inc.php
@@ -0,0 +1,6 @@
+<!-- Footer starts here -->
+<div class="footer">
+    <p>© 2012, IMI Mobile Private Limited </p></div>
+<!-- Footer Ends here -->
+</body>
+</html>
---
+++ head_foot/header_1.inc.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PFI Monitor2</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/style.css" type="text/css">
+</head>
+<script type="text/javascript" src="fusioncharts/fusioncharts.js"></script>
+<script type="text/javascript" src="fusioncharts/themes/fusioncharts.theme.carbon.js"></script>
+<script type="text/javascript" src="fusioncharts/themes/fusioncharts.theme.ocean.js"></script>
+<script src="/js/jquery-2.1.1.min.js"></script>
+
+<body> <!-- onload="JavaScript:AutoRefresh(5000);"> -->
+<a href="/" id="banner"><img src="/images/IMImobile_logo.png" alt="banner" class="banner"></a>
+<div id="topbanner">
+    <ul id="menu">
+        <li><a href="/" style="text-decoration: none" >Overview</a></li>
+        <li><a href="/" style="text-decoration: none" >Transactions ESC</a></li>
+        <li><a href="/" style="text-decoration: none" >Transactions SC</a></li>
+        <li><a href="/" style="text-decoration: none" >Sites</a></li>
+        <li><a href="/" style="text-decoration: none" >MO</a></li>
+        <li><a href="/" style="text-decoration: none" >PSMS/MT</a></li>
+        <li><a href="/blaa.txt" style="text-decoration: none" >Servers</a></li>
+    </ul>
+</div>
+<div id="lowerbanner"></div>
+<!-- BODY CONTENT BELOW-->
