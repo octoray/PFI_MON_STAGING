48bb7df9dbfc1d9559a11d63b33b6c5f2811037a
davidwintour <david.wintour@gmail.com>
2015-01-06 12:35:26 +0000

    sql link test

--- php/sql_connect.php
+++ php/sql_connect.php
@@ -1,15 +1,24 @@
 <?php
-
-// Server in the this format: <computer>\<instance name> or
-// <server>,<port> when using a non default port number
-$server = 'boron.winprod.lan\11500';
-
-// Connect to MSSQL
-$link = mssql_connect($server, 'TSG', 'K33pM0ving');
-
-if (!$link) {
-    die('Something went wrong while connecting to MSSQL');
-}else{
-    echo "connected fine :)";
+try {
+//$con = new PDO('odbc:Driver=FreeTDS; Server=full_machinename_or_ip_address\
+//   (servername_if_any); Database=db_to_connect; UID=username;PWD=password;);
+$con = new PDO('odbc:Driver=FreeTDS; Server=remote_server\mssqlserver;
+Database=db_students; UID=db_user; PWD=password123;');
+// Now you have connection object. You can query on your db.
+// Example to execute ful SQL query
+$result = $con->query('SELECT Name FROM dbo.students');
+foreach ($result as $row) {
+print $row['Name'] . '<br />';
 }
+
+// Example to execute stored procedure
+$result = $con->query('EXEC dbo.GetAllStudents');
+foreach ($result as $row) {
+print 'Name: ' . $row['Name'] . ' Grade: ' + $row['Grade'] . '<br />';
+}
+$con = null;
+} catch (PDOException $e) {
+echo $e->getMessage();
+}
+
 ?>
