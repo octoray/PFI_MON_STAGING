0cb800cd5f2162412e56a714fb62f38fd6710621
davidwintour <david.wintour@gmail.com>
2015-01-06 12:10:50 +0000

    sql link test

--- php/sql_connect.php
+++ php/sql_connect.php
@@ -1,12 +1,15 @@
 <?php
-$serverName = "boron.winprod.lan\sqlexpress, 11500"; //serverName\instanceName, portNumber (default is 1433)
-$connectionInfo = array( "Database"=>"PayForIt", "UID"=>"TSG", "PWD"=>"K33pM0ving");
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
 
-if( $conn ) {
-    echo "Connection established.<br />";
+// Server in the this format: <computer>\<instance name> or
+// <server>,<port> when using a non default port number
+$server = 'boron.winprod.lan\11500';
+
+// Connect to MSSQL
+$link = mssql_connect($server, 'TSG', 'K33pM0ving');
+
+if (!$link) {
+    die('Something went wrong while connecting to MSSQL');
 }else{
-    echo "Connection could not be established.<br />";
-    die( print_r( sqlsrv_errors(), true));
+    echo "connected fine :)";
 }
 ?>
