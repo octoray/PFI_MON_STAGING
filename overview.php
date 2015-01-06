d635f99c1a34e88f9a8cca0bb95bfec27452fc9d
davidwintour <david.wintour@gmail.com>
2015-01-06 18:08:24 +0000

    css change

--- overview.php
+++ overview.php
@@ -2,27 +2,5 @@
 
 <div class="wrapper">
 
-    <table id="t01">
-        <tr>
-            <th>Time</th>
-            <th>Success</th>
-            <th>Failed</th>
-        </tr>
-        <tr>
-            <td>14:00</td>
-            <td>305</td>
-            <td>23</td>
-        </tr>
-        <tr>
-            <td>13:50</td>
-            <td>567</td>
-            <td>11</td>
-        </tr>
-
-</div>
-<br>
-<br>
-<br>
-<br>
 
 <?php require("./head_foot/footer_1.inc.php"); ?>
