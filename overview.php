575edef102f798356868a984a42da9c114c03830
davidwintour <david.wintour@gmail.com>
2015-01-06 18:14:16 +0000

    css change

--- overview.php
+++ overview.php
@@ -1,9 +1,31 @@
 <?php require("./head_foot/header_1.inc.php"); ?>
 
 <div class="wrapper">
-    <h1>test1</h1>
-    <h2>test2</h2>
-    <h3>test3</h3>
+    <br>
+    <h1>Transactions: Success Vs Failed</h1>
+
+    <table id="t01">
+        <tr>
+            <th>Time</th>
+            <th>Success Count</th>
+            <th>Fail Count</th>
+        </tr>
+        <tr>
+            <td>14:00</td>
+            <td>306</td>
+            <td>11</td>
+        </tr>
+        <tr>
+            <td>13:50</td>
+            <td>456</td>
+            <td>32</td>
+        </tr>
+    </table>
+
+    <h1>Health</h1>
+
+    Good
+
 </div>
 
 <?php require("./head_foot/footer_1.inc.php"); ?>
