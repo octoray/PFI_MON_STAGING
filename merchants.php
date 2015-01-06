d6d1c0543f5c22f50b7712883e9bd703e3c09631
davidwintour <david.wintour@gmail.com>
2015-01-06 18:00:58 +0000

    change

--- merchants.php
+++ merchants.php
@@ -1,7 +1,16 @@
 <?php require("./head_foot/header_1.inc.php"); ?>
-
+<script>
+    setTimeout(function(){
+        window.location.reload(true);
+    }, 60000);
+</script>
 <div class="wrapper">
+    <p class="date" align="center">TSG PFI Monitors</p><br><br>
 
+    <div class="center">
+        <iframe src="http://wintoday.77win.co" width="450" height="600" frameborder="0"></iframe>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <iframe src="http://tsgpfi.co.uk/" width="450" height="600" frameborder="0"></iframe>
+    </div>
 </div>
 
 <?php require("./head_foot/footer_1.inc.php"); ?>
--- overview.php
+++ overview.php
@@ -2,6 +2,29 @@
 
 <div class="wrapper">
 
+    <h1>Health</h1>
+
+    <h1>Transaction Counts Success Vs failed</h1>
+
+    <table id="t01">
+        <tr>
+            <th>Time</th>
+            <th>Success</th>
+            <th>Failed</th>
+        </tr>
+        <tr>
+            <td>14:00</td>
+            <td>305</td>
+            <td>23</td>
+        </tr>
+        <tr>
+            <td>13:50</td>
+            <td>567</td>
+            <td>11</td>
+        </tr>
+
+    <h1>Health</h1>
+
 </div>
 
 <?php require("./head_foot/footer_1.inc.php"); ?>
