3b64a1b3926c4006fe4cf79a61b622631132aded
octoray <david.wintour@gmail.com>
2015-01-11 17:42:25 +0000

    First add of node dbbridge

---
+++ dbbridge/dbbridge.js
@@ -0,0 +1,21 @@
+var sql = require('mssql'); 
+var mysql = require('mysql');
+var fs = require('fs');
+
+eval(fs.readFileSync('./config/dbconfig.conf')+''); //Load DB config
+eval(fs.readFileSync('./classes/dbquerys.module')+''); //Load DB Querys
+eval(fs.readFileSync('./classes/dbfunctions.module')+''); //Load DB functions
+
+
+// DB BRIDGE QUERYS
+
+sql.connect(config, function(err) {
+    if (err)
+        throw (err);
+
+    myFunction();
+    myFunction2();
+
+    // Query
+    sql.close();
+});
