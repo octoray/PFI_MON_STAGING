65fff10bff3d8a9c40481a3d90f10622d5b701b2
davidwintour <david.wintour@gmail.com>
2015-01-11 18:20:47 +0000

    ssql

--- php/imsi_count.php
+++ php/imsi_count.php
@@ -7,4 +7,17 @@ $hostname = "localhost";
 $dbhandle = mysql_connect($hostname, $username, $password)
 or die("Unable to connect to MySQL");
 echo "Connected to MySQL<br>";
+
+mysql_select_db("PFI_MON") or die(mysql_error());
+
+$result = mysql_query("SELECT * FROM test")
+or die(mysql_error());
+
+
+// store the record of the "example" table into $row
+$row = mysql_fetch_array( $result );
+// Print out the contents of the entry
+
+echo "Name: ".$row['name'];
+echo " Age: ".$row['age'];
 ?>
