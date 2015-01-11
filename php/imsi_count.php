f64244077d11c7f657ed0ead48d3ddaf6ff71866
davidwintour <david.wintour@gmail.com>
2015-01-11 18:08:32 +0000

    css change

--- php/imsi_count.php
+++ php/imsi_count.php
@@ -1,23 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "pfimonuser";
-$password = "Fa6rUCha";
-$dbname = "PFI_MON";
+$username = "your_name";
+$password = "your_password";
+$hostname = "localhost";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "select * from test where id = '1'";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password)
+or die("Unable to connect to MySQL");
+echo "Connected to MySQL<br>";
 ?>
