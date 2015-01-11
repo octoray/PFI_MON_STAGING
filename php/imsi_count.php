988e17b26a08f8f31627430ef45f71265dc415e0
davidwintour <david.wintour@gmail.com>
2015-01-11 18:01:27 +0000

    sql

---
+++ php/imsi_count.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "pfimonuser";
+$password = "Fa6rUCha";
+$dbname = "PFI_MON";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "select * from test where id = '1'";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
