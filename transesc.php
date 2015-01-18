f98a127e8d800c729a86c3c8debc60499ec3193f
davidwintour <david.wintour@gmail.com>
2015-01-18 16:58:15 +0000

    css change

--- transesc.php
+++ transesc.php
@@ -1,6 +1,10 @@
 <?php require("./head_foot/header_1.inc.php"); ?>
+<script type="text/javascript" src="fusioncharts/charts/esc_trans.js"></script>
 
 <div class="wrapper">
+
+    <div id="chartContainer_header" align="center" class="chart2"> SC Transactions Last Hour </div>
+    <div id="chartContainer2" align="center"></div>
 
 </div>
 
