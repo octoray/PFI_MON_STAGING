7966372d7592c31124dc971a83d06ffd05371d46
davidwintour <david.wintour@gmail.com>
2015-01-18 17:48:15 +0000

    css change

--- transesc.php
+++ transesc.php
@@ -10,7 +10,7 @@
             dataFormat: "json",
             dataSource: {
                 "chart": {
-                    "caption": "Single Click Transactions Last Hour",
+                    "caption": "ESC Transactions Last Hour",
                     "subCaption": "SC",
                     "xAxisName": "Time",
                     "yAxisName": "Amount",
@@ -23,37 +23,33 @@
                 "data": [
                     {
                         "label": "'.$time1.'",
-                        "value": "320000"
+                        "value": "'.$amount6.'"
                     },
                     {
-                        "label": "14:10",
-                        "value": "110000"
+                        "label": "'.$time5.'",
+                        "value": "'.$amount5.'"
                     },
                     {
-                        "label": "14:20",
-                        "value": "720000"
+                        "label": "'.$time4.'",
+                        "value": "'.$amount4.'"
                     },
                     {
-                        "label": "14:30",
-                        "value": "190000"
+                        "label": "'.$time3.'",
+                        "value": "'.$amount3.'"
                     },
                     {
-                        "label": "14:40",
-                        "value": "490000"
+                        "label": "'.$time2.'",
+                        "value": "'.$amount2.'"
                     },
                     {
-                        "label": "14:50",
-                        "value": "310000"
-                    },
-                    {
-                        "label": "15:00",
-                        "value": "280000"
+                        "label": "'.$time1.'",
+                        "value": "'.$amount1.'"
                     }
                 ]
             }
 
         });
-        revenueChart.render("chartContainer2");
+        revenueChart.render("chartContainer");
     });'
 ?>
 
@@ -61,8 +57,8 @@
 
 <div class="wrapper">
 
-    <div id="chartContainer_header" align="center" class="chart2"> SC Transactions Last Hour </div>
-    <div id="chartContainer2" align="center"></div>
+    <div id="chartContainer_header" align="center" class="chart2"> ESC Transactions Last Hour </div>
+    <div id="chartContainer" align="center"></div>
 
 </div>
 
