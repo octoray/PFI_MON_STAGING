aefde033d5c567e94247fc8495d9c4be935b4019
davidwintour <david.wintour@gmail.com>
2015-01-18 18:57:28 +0000

    css change

--- head_foot/footer_1.inc.php
+++ head_foot/footer_1.inc.php
@@ -1,6 +1,6 @@
 <!-- Footer starts here -->
 <div class="footer">
-    <p>© 2012, IMI Mobile Private Limited </p></div>
+    <p>© 2015, IMI Mobile Private Limited </p></div>
 <!-- Footer Ends here -->
 </body>
 </html>
