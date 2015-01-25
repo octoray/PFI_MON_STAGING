6bb085532fa2d4fbcc5c328a37fa84e572614682
davidwintour <david.wintour@gmail.com>
2015-01-25 13:31:10 +0000

    css change

--- marketing.php
+++ marketing.php
@@ -3,6 +3,6 @@
     <br>
     <h1>Sorry, not finished yet. To make up for it enjoy this funny MEME :)</h1>
     <br>
-    <a href="/"><img src="/images/wow-26.jpg"></a>
+    <a href="/"><img src="/images/show-you-the-world.jpg"></a>
 </div>
 <?php require("./head_foot/footer_1.inc.php"); ?>
