6fe00b996dd41bb665a6281c04b8749b5dc0ce7b
davidwintour <david.wintour@gmail.com>
2015-01-25 20:41:16 +0000

    css change

--- admin.php
+++ admin.php
@@ -1,7 +1,7 @@
 <?php require("./head_foot/header_1.inc.php"); ?>
 <div class="wrapper">
 
-        <iframe src="http://tsgpfi.co.uk/" width="1500" height="750" frameborder="0"></iframe>
+        <iframe src="http://mh-pfi-mon-01.win.local:10000" width="1500" height="750" frameborder="0"></iframe>
 
 </div>
 <?php require("./head_foot/footer_1.inc.php"); ?>
