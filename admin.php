91f47869e0eb92fb5dd49cba579580be1b1b1661
davidwintour <david.wintour@gmail.com>
2015-01-25 20:36:01 +0000

    css change

---
+++ admin.php
@@ -0,0 +1,5 @@
+<?php require("./head_foot/header_1.inc.php"); ?>
+<div class="wrapper">
+    <iframe src="http://mh-pfi-mon-01.win.local:10000/" width="800" height="600" frameborder="0"></iframe>
+</div>
+<?php require("./head_foot/footer_1.inc.php"); ?>
