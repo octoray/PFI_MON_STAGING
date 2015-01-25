810edfbe92ed3bf3a20822949ad7bc26e0ec122f
davidwintour <david.wintour@gmail.com>
2015-01-25 20:49:54 +0000

    css change

--- admin.php
+++ admin.php
@@ -1,6 +1,6 @@
 <?php require("./head_foot/header_1.inc.php"); ?>
 <div class="wrapper">
-<br>
+<br><br>
     <a href="http://mh-pfi-mon-01.win.local:10000/" id="banner"><img src="/images/Webmin-Logo-600.png"></a>
     <br>
     <a href="http://mh-pfi-mon-01.win.local:10000/">WEBMIN</a>
