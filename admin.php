f1ccf529f1dd5723f0f6f713f14ef59bb2aad48b
davidwintour <david.wintour@gmail.com>
2015-01-25 20:43:19 +0000

    css change

--- admin.php
+++ admin.php
@@ -1,7 +1,7 @@
 <?php require("./head_foot/header_1.inc.php"); ?>
 <div class="wrapper">
-
-        <iframe src="http://mh-pfi-mon-01.win.local:10000" width="1500" height="750" frameborder="0"></iframe>
+<br>
+    <a href="http://mh-pfi-mon-01.win.local:10000/">WEBMIN</a>
 
 </div>
 <?php require("./head_foot/footer_1.inc.php"); ?>
