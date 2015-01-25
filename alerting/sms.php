a67ada597426b8692b95bc24182192d2ce382bb8
davidwintour <david.wintour@gmail.com>
2015-01-25 11:39:31 +0000

    css change

--- alerting/sms.php
+++ alerting/sms.php
@@ -3,7 +3,7 @@ $url = 'http://172.16.10.31:8081/win-smsgwweb/winmt';
 //extract($_POST);
 
 //$url = $_POST["url"];
-
+$c1_time6 = 'bob';
 //$ch = curl_init($url);
 //curl_setopt($ch, CURLOPT_HEADER, 0);
 //curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -11,13 +11,13 @@ $url = 'http://172.16.10.31:8081/win-smsgwweb/winmt';
 //curl_setopt($ch, CURLOPT_URL, $url);
 //curl_setopt($ch, CURLOPT_POST, 1);
 
-$new = htmlspecialchars("<?xml version=\"1.0\" standalone=\"no\"?>
+$new = "<?xml version=\"1.0\" standalone=\"no\"?>
 <!DOCTYPE WIN_DELIVERY_2_SMS SYSTEM \"winbound_messages_v1.dtd\">
 <WIN_DELIVERY_2_SMS>
   <!-- E.g. minimal set of elements -->
   <SMSMESSAGE>
     <DESTINATION_ADDR>+14088396711</DESTINATION_ADDR>
-    <TEXT><IMI OPS TEST MESSAGE></TEXT>
+    <TEXT><".$c1_time6."></TEXT>
     <TRANSACTIONID>111222371</TRANSACTIONID>
     <TYPEID>2</TYPEID>
     <SERVICEID>1</SERVICEID>
