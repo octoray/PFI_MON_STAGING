74a96c41bc06ab0bbc66656bbf95e4868af447de
davidwintour <david.wintour@gmail.com>
2015-01-27 15:35:12 +0000

    css change

--- alerting/phpmailer/send.php
+++ alerting/phpmailer/send.php
@@ -7,7 +7,7 @@ $mail = new PHPMailer;
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = false;                               // Enable SMTP authentication
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'david.wintour@gmail.com';                 // SMTP username
 $mail->Password = 'Rtfm123456789';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
