8285f3ecf4c860167b9f17c502791157f52cd14c
davidwintour <david.wintour@gmail.com>
2015-01-27 18:09:45 +0000

    css change

--- alerting/phpmailer/send.php
+++ alerting/phpmailer/send.php
@@ -35,7 +35,7 @@ $mail->Body    = '<!DOCTYPE html>
     <meta charset="UTF-8">
 </head>
 <body>
-<img src="http://www.tsgpficontent.co.uk/images/IMImobile_logo.png align="middle">
+<img src="http://www.tsgpficontent.co.uk/images/IMImobile_logo.png" align="middle">
 <b>PFI Alert Test</b>
 <p>Test alert 1</p>
 </body>
