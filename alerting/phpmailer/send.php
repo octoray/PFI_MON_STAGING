8ae3d16faba8929c781cbdfd7a45a440733db49d
davidwintour <david.wintour@gmail.com>
2015-01-27 14:52:31 +0000

    css change

--- alerting/phpmailer/send.php
+++ alerting/phpmailer/send.php
@@ -6,17 +6,18 @@ $mail = new PHPMailer;
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Host = '192.168.60.120';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = false;                               // Enable SMTP authentication
 $mail->Username = 'david.wintour@gmail.com';                 // SMTP username
 $mail->Password = 'Rtfm123456789';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+$mail->Port = 25;                                    // TCP port to connect to
 
 $mail->From = 'david.wintour@gmail.com';
 $mail->FromName = 'test';
 $mail->addAddress('david.wintour@imimobile.com', 'Joe User');     // Add a recipient
-$mail->addAddress('david.wintour@imimobile.com');               // Name is optional
+$mail->addAddress('david.wintour@imimobile.com');
+$mail->addAddress('davidwintour@imimobile.com');     // Name is optional
 // $mail->addReplyTo('info@example.com', 'Information');
 $mail->addCC('tsg@imimobile.com');
 //$mail->addBCC('bcc@example.com');
@@ -26,7 +27,7 @@ $mail->addCC('tsg@imimobile.com');
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->Body    = 'test dw 2 This is the HTML message body <b>in bold!</b>';
 $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 if(!$mail->send()) {
