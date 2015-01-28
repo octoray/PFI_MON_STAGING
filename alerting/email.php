8762f84c4ac0b49747794cd47b907e49c074b80f
davidwintour <david.wintour@gmail.com>
2015-01-28 20:01:39 +0000

    css change

--- alerting/email.php
+++ alerting/email.php
@@ -8,11 +8,11 @@ $mail = new PHPMailer;
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = '192.168.60.120';  // Specify main and backup SMTP servers
-//$mail->SMTPAuth = false;                               // Enable SMTP authentication
-//$mail->Username = 'david.wintour@gmail.com';                 // SMTP username
-//$mail->Password = 'Rtfm123456789';                           // SMTP password
-//$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'david.wintour@gmail.com';                 // SMTP username
+$mail->Password = 'Rtfm123456789';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 25;                                    // TCP port to connect to
 
 $mail->From = 'david.wintour@gmail.com';
@@ -36,8 +36,6 @@ $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 if(!$mail->send()) {
     echo 'Message could not be sent.';
     echo 'Mailer Error: ' . $mail->ErrorInfo;
-    echo "<br>";
-    print_r($mail);
 } else {
     echo 'Message has been sent';
 }
