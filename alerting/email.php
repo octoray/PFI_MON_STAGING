c549c4883f0b609b3bfd42aa3992e4cb2d8f3e03
davidwintour <david.wintour@gmail.com>
2015-01-28 14:54:09 +0000

    css change

--- alerting/email.php
+++ alerting/email.php
@@ -72,6 +72,8 @@ $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 if(!$mail->send()) {
     echo 'Message could not be sent.';
     echo 'Mailer Error: ' . $mail->ErrorInfo;
+    echo "<br>";
+    print_r($mail);
 } else {
     echo 'Message has been sent';
 }
