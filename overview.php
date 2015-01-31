795d65f2557b9e1ac9bba64ec613385e3564beb2
davidwintour <david.wintour@gmail.com>
2015-01-31 12:46:36 +0000

    css change

--- overview.php
+++ overview.php
@@ -33,25 +33,25 @@
             <td>Unkown Error</td>
             <td style="color: black;"><?php echo $row3['amount']; ?></td>
             <td style="color: black;"><?php echo $row3['time']; ?></td>
-            <td style="color: black;"><img src="/images/button_glossy_red.png" width="25%" height="30%"></td>
+            <td style="color: black;"><<?php warning_error($row3['amount']); ?>></td>
         </tr>
         <tr>
             <td>Processing Payment, Pending</td>
             <td style="color: black;"><?php echo $row4['amount']; ?></td>
             <td style="color: black;"><?php echo $row4['time']; ?></td>
-            <td style="color: black;"><img src="/images/button_glossy_green.png" width="25%" height="30%"></td>
+            <td style="color: black;"><<?php warning_error($row4['amount']); ?>></td>
         </tr>
         <tr>
             <td>Failed At Network</td>
             <td style="color: black;"><?php echo $row5['amount']; ?></td>
             <td style="color: black;"><?php echo $row5['time']; ?></td>
-            <td style="color: black;"><img src="/images/button_glossy_yellow.png" width="25%" height="30%"></td>
+            <td style="color: black;"><<?php warning_error($row5['amount']); ?>></td>
         </tr>
         <tr>
             <td>Network Error</td>
             <td style="color: black;"><?php echo $row6['amount']; ?></td>
             <td style="color: black;"><?php echo $row6['time']; ?></td>
-            <td style="color: black;"><img src="/images/button_glossy_green.png" width="25%" height="30%"></td>
+            <td style="color: black;"><<?php warning_error($row6['amount']); ?>></td>
         </tr>
     </table>
 
@@ -62,36 +62,43 @@
             <th>Status</th>
             <th>Amount</th>
             <th>Last Updated</th>
+            <th>Warning</th>
         </tr>
         <tr>
             <td>MO Successfully Recived</td>
             <td style="color: black;"><?php echo $row7['amount']; ?></td>
             <td style="color: black;"><?php echo $row7['time']; ?></td>
+            <td style="color: black;"><<?php warning_success($row7['amount']); ?>></td>
         </tr>
         <tr>
             <td>MT's Delivered</td>
             <td style="color: black;"><?php echo $row8['amount']; ?></td>
             <td style="color: black;"><?php echo $row8['time']; ?></td>
+            <td style="color: black;"><<?php warning_success($row8['amount']); ?>></td>
         </tr>
         <tr>
             <td>Sent MT, No Final DR Yet</td>
             <td style="color: black;"><?php echo $row9['amount']; ?></td>
             <td style="color: black;"><?php echo $row9['time']; ?></td>
+            <td style="color: black;"><<?php warning_error($row9['amount']); ?>></td>
         </tr>
         <tr>
             <td>underlying connection forcibly closed</td>
             <td style="color: black;"><?php echo $row10['amount']; ?></td>
             <td style="color: black;"><?php echo $row10['time']; ?></td>
+            <td style="color: black;"><<?php warning_error($row10['amount']); ?>></td>
         </tr>
         <tr>
             <td>The request was aborted</td>
             <td style="color: black;"><?php echo $row11['amount']; ?></td>
             <td style="color: black;"><?php echo $row11['time']; ?></td>
+            <td style="color: black;"><<?php warning_error($row11['amount']); ?>></td>
         </tr>
         <tr>
             <td>The operation has timed out</td>
             <td style="color: black;"><?php echo $row12['amount']; ?></td>
             <td style="color: black;"><?php echo $row12['time']; ?></td>
+            <td style="color: black;"><<?php warning_error($row12['amount']); ?>></td>
         </tr>
     </table>
 
@@ -103,60 +110,70 @@
             <th>Status</th>
             <th>Amount</th>
             <th>Last Updated</th>
+            <th>Warning</th>
         </tr>
         <tr>
             <td>ESC Transactions</td>
             <td>Pending/Stuck Transactions</td>
             <td style="color: black;"><?php echo $row13['amount']; ?></td>
             <td style="color: black;"><?php echo $row13['time']; ?></td>
+            <td style="color: black;"><<?php warning_error_pendtrans($row13['amount']); ?>></td>
         </tr>
         <tr>
             <td>ESC SIte Traffic</td>
             <td>ESC Site Hits</td>
             <td style="color: black;"><?php echo $row14['amount']; ?></td>
             <td style="color: black;"><?php echo $row14['time']; ?></td>
+            <td style="color: black;"><<?php warning_error_sitehits($row14['amount']); ?>></td>
         </tr>
         <tr>
             <td>ESC</td>
             <td>Screenshots</td>
             <td style="color: black;"><?php echo $row15['amount']; ?></td>
             <td style="color: black;"><?php echo $row15['time']; ?></td>
+            <td style="color: black;"><<?php warning_success($row15['amount']); ?>></td>
         </tr>
         <tr>
             <td>IMSI</td>
             <td>Successful IMSI</td>
             <td style="color: black;"><?php echo $row16['amount']; ?></td>
             <td style="color: black;"><?php echo $row16['time']; ?></td>
+            <td style="color: black;"><<?php warning_success($row16['amount']); ?>></td>
         </tr>
         <tr>
             <td>IMSI</td>
             <td>Failed IMSI</td>
             <td style="color: black;"><?php echo $row17['amount']; ?></td>
             <td style="color: black;"><?php echo $row17['time']; ?></td>
+            <td style="color: black;"><<?php warning_error($row17['amount']); ?>></td>
         </tr>
         <tr>
             <td>STOPS</td>
             <td>Successful STOPS</td>
             <td style="color: black;"><?php echo $row18['amount']; ?></td>
             <td style="color: black;"><?php echo $row18['time']; ?></td>
+            <td style="color: black;"><<?php warning_success($row18['amount']); ?>></td>
         </tr>
         <tr>
             <td>STOPS</td>
             <td>Failed STOPS</td>
             <td style="color: black;"><?php echo $row19['amount']; ?></td>
             <td style="color: black;"><?php echo $row19['time']; ?></td>
+            <td style="color: black;"><<?php warning_error($row19['amount']); ?>></td>
         </tr>
         <tr>
             <td>Notifications</td>
             <td>Successfully Sent Notifications</td>
             <td style="color: black;"><?php echo $row20['amount']; ?></td>
             <td style="color: black;"><?php echo $row20['time']; ?></td>
+            <td style="color: black;"><<?php warning_success($row20['amount']); ?>></td>
         </tr>
         <tr>
             <td>Notifications</td>
             <td>Failed To Send Notifications</td>
             <td style="color: black;"><?php echo $row21['amount']; ?></td>
             <td style="color: black;"><?php echo $row21['time']; ?></td>
+            <td style="color: black;"><<?php warning_error($row21['amount']); ?>></td>
         </tr>
     </table>
 </div>
--- php/warning.php
+++ php/warning.php
@@ -17,4 +17,24 @@ function warning_error($result) {
         echo 'img src="/images/button_glossy_red.png" width="25%" height="30%"';
     }};
 
+
+function warning_error_pendtrans($result) {
+    if($result < "5"){
+        echo 'img src="/images/button_glossy_green.png" width="25%" height="30%"';
+    } elseif ($result < "15"){
+        echo 'img src="/images/button_glossy_yellow.png" width="25%" height="30%"';
+    }elseif($result > "14"){
+        echo 'img src="/images/button_glossy_red.png" width="25%" height="30%"';
+    }};
+
+
+function warning_error_sitehits($result) {
+    if($result < "100"){
+        echo 'img src="/images/button_glossy_red.png" width="25%" height="30%"';
+    } elseif ($result < "250"){
+        echo 'img src="/images/button_glossy_yellow.png" width="25%" height="30%"';
+    }elseif($result > "249"){
+        echo 'img src="/images/button_glossy_green.png" width="25%" height="30%"';
+    }};
+
 ?>
