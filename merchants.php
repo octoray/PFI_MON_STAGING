75bd36480b3befaf8fd8761186f41ad8e6fb0f27
davidwintour <david.wintour@gmail.com>
2015-02-05 15:35:32 +0000

    css change

--- merchants.php
+++ merchants.php
@@ -1,8 +1,7 @@
 <?php require("./head_foot/header_1.inc.php"); ?>
 <div class="wrapper">
     <br>
-    <h1>Sorry, not finished yet. To make up for it enjoy this funny MEME :)</h1>
+    <h1>Sorry, not finished yet.</h1>
     <br>
-    <a href="/"><img src="/images/wow-26.jpg"></a>
 </div>
 <?php require("./head_foot/footer_1.inc.php"); ?>
