4ecabff4eca8083c8f5630bcc39717ee6783a77f
davidwintour <david.wintour@gmail.com>
2015-02-15 17:54:19 +0000

    css change

--- js/chnagecolour.js
+++ js/chnagecolour.js
@@ -3,8 +3,48 @@ $(document).ready(function(){
     if (number >= "9") {
         $(".wrapper").css("background-color", "yellow");
     }
-    var number2 = parseInt($("#fail2").text());
+    var number1 = parseInt($("#fail2").text());
+    if (number1 >= "9") {
+        $(".wrapper").css("background-color", "yellow");
+    }
+    var number2 = parseInt($("#fail3").text());
     if (number2 >= "9") {
+        $(".wrapper").css("background-color", "yellow");
+    }
+    var number3 = parseInt($("#fail4").text());
+    if (number3 >= "9") {
+        $(".wrapper").css("background-color", "yellow");
+    }
+    var number4 = parseInt($("#fail5").text());
+    if (number4 >= "9") {
+        $(".wrapper").css("background-color", "yellow");
+    }
+    var number5 = parseInt($("#fail6").text());
+    if (number5 >= "9") {
+        $(".wrapper").css("background-color", "yellow");
+    }
+    var number6 = parseInt($("#fail7").text());
+    if (number6 >= "9") {
+        $(".wrapper").css("background-color", "yellow");
+    }
+    var number7 = parseInt($("#fail8").text());
+    if (number7 >= "9") {
+        $(".wrapper").css("background-color", "yellow");
+    }
+    var number8 = parseInt($("#fail9").text());
+    if (number8 >= "9") {
+        $(".wrapper").css("background-color", "yellow");
+    }
+    var number9 = parseInt($("#fail10").text());
+    if (number9 >= "9") {
+        $(".wrapper").css("background-color", "yellow");
+    }
+    var number10 = parseInt($("#fail11").text());
+    if (number10 >= "9") {
+        $(".wrapper").css("background-color", "yellow");
+    }
+    var number11 = parseInt($("#fail12").text());
+    if (number11 >= "9") {
         $(".wrapper").css("background-color", "yellow");
     }
 });
