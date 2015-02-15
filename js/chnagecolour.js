42a0340ddedc8b089b56a00279b94e2a59de93b3
davidwintour <david.wintour@gmail.com>
2015-02-15 16:25:57 +0000

    css change

--- js/chnagecolour.js
+++ js/chnagecolour.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     if ($("#bsuc").text() < "1") {
         $(".wrapper").css("background-color", "yellow");
     };
-    if ($("#jim").text() > "5") {
+    if ($("#jim").text() > "10") {
         $(".wrapper").css("background-color", "yellow");
     };
 });
