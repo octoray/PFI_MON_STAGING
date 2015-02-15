02d7d03096cf3e56cfc4a8d70db1e4740ddb7a6b
davidwintour <david.wintour@gmail.com>
2015-02-15 16:44:39 +0000

    css change

--- js/chnagecolour.js
+++ js/chnagecolour.js
@@ -2,40 +2,40 @@ $(document).ready(function(){
     if ($("#bsuc").text() < "1") {
         $(".wrapper").css("background-color", "yellow");
     }
-    if ($("#fail1").text() > "9") {
+    if ($("#fail1").text() >= "9") {
         $(".wrapper").css("background-color", "yellow");
     }
-    if ($("#fail2").text() > "9") {
+    if ($("#fail2").text() >= "9") {
         $(".wrapper").css("background-color", "yellow");
     }
-    if ($("#fail3").text() > "9") {
+    if ($("#fail3").text() >= "9") {
         $(".wrapper").css("background-color", "yellow");
     }
-    if ($("#fail4").text() > "9") {
+    if ($("#fail4").text() >= "9") {
         $(".wrapper").css("background-color", "yellow");
     }
-    if ($("#fail5").text() > "9") {
+    if ($("#fail5").text() >= "9") {
         $(".wrapper").css("background-color", "yellow");
     }
-    if ($("#fail6").text() > "9") {
+    if ($("#fail6").text() >= "9") {
         $(".wrapper").css("background-color", "yellow");
     }
-    if ($("#fail7").text() > "9") {
+    if ($("#fail7").text() >= "9") {
         $(".wrapper").css("background-color", "yellow");
     }
-    if ($("#fail8").text() > "9") {
+    if ($("#fail8").text() >= "9") {
         $(".wrapper").css("background-color", "yellow");
     }
-    if ($("#fail9").text() > "9") {
+    if ($("#fail9").text() >= "9") {
         $(".wrapper").css("background-color", "yellow");
     }
-    if ($("#fail10").text() > "9") {
+    if ($("#fail10").text() >= "9") {
         $(".wrapper").css("background-color", "yellow");
     }
-    if ($("#fail11").text() > "9") {
+    if ($("#fail11").text() >= "9") {
         $(".wrapper").css("background-color", "yellow");
     }
-    if ($("#fail12").text() > "9") {
+    if ($("#fail12").text() >= "9") {
         $(".wrapper").css("background-color", "yellow");
     }
 });
