93c02b6e618d6d08dea75a5ce05a95479cc014bc
davidwintour <david.wintour@gmail.com>
2015-02-15 17:50:43 +0000

    css change

--- js/chnagecolour.js
+++ js/chnagecolour.js
@@ -1,53 +1,10 @@
 $(document).ready(function(){
-    if ($("#bsuc").text() < "1") {
-        $(".wrapper").css("background-color", "yellow");
-    }
-    var number1 = parseInt($("#fail1").text());
-    if (number1 >= "9") {
+    var number = parseInt($("#fail1").text());
+    if (number >= "9") {
         $(".wrapper").css("background-color", "yellow");
     }
     var number2 = parseInt($("#fail2").text());
-    if if (number2 >= "9") {
-        $(".wrapper").css("background-color", "yellow");
-    }
-    var number3 = parseInt($("#fail3").text());
-    if if (number3 >= "9") {
-        $(".wrapper").css("background-color", "yellow");
-    }
-    var number4 = parseInt($("#fail4").text());
-    if if (number4 >= "9") {
-        $(".wrapper").css("background-color", "yellow");
-    }
-    var number5 = parseInt($("#fail5").text());
-    if if (number5 >= "9") {
-        $(".wrapper").css("background-color", "yellow");
-    }
-    var number6 = parseInt($("#fail6").text());
-    if if (number6 >= "9") {
-        $(".wrapper").css("background-color", "yellow");
-    }
-    var number7 = parseInt($("#fail7").text());
-    if if (number7 >= "9") {
-        $(".wrapper").css("background-color", "yellow");
-    }
-    var number8 = parseInt($("#fail8").text());
-    if if (number8 >= "9") {
-        $(".wrapper").css("background-color", "yellow");
-    }
-    var number9 = parseInt($("#fail9").text());
-    if if (number9 >= "9") {
-        $(".wrapper").css("background-color", "yellow");
-    }
-    var number10 = parseInt($("#fail10").text());
-    if if (number10 >= "9") {
-        $(".wrapper").css("background-color", "yellow");
-    }
-    var number11 = parseInt($("#fail11").text());
-    if if (number11 >= "9") {
-        $(".wrapper").css("background-color", "yellow");
-    }
-    var number12 = parseInt($("#fail12").text());
-    if if (number12 >= "9") {
+    if (number2 >= "9") {
         $(".wrapper").css("background-color", "yellow");
     }
 });
