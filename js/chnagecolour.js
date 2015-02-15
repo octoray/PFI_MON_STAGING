c8544b6f59d1c489987fac1be89033b7a805d807
davidwintour <david.wintour@gmail.com>
2015-02-15 16:33:25 +0000

    css change

--- js/chnagecolour.js
+++ js/chnagecolour.js
@@ -1,41 +1,41 @@
 $(document).ready(function(){
     if ($("#bsuc").text() < "1") {
         $(".wrapper").css("background-color", "yellow");
-    };
+    }
     if ($("#fail1").text() > "9") {
         $(".wrapper").css("background-color", "yellow");
-    };
+    }
     if ($("#fail2").text() > "9") {
         $(".wrapper").css("background-color", "yellow");
-    };
+    }
     if ($("#fail3").text() > "9") {
         $(".wrapper").css("background-color", "yellow");
-    };
+    }
     if ($("#fail4").text() > "9") {
         $(".wrapper").css("background-color", "yellow");
-    };
+    }
     if ($("#fail5").text() > "9") {
         $(".wrapper").css("background-color", "yellow");
-    };
+    }
     if ($("#fail6").text() > "9") {
         $(".wrapper").css("background-color", "yellow");
-    };
+    }
     if ($("#fail7").text() > "9") {
         $(".wrapper").css("background-color", "yellow");
-    };
+    }
     if ($("#fail8").text() > "9") {
         $(".wrapper").css("background-color", "yellow");
-    };
+    }
     if ($("#fail9").text() > "9") {
         $(".wrapper").css("background-color", "yellow");
-    };
+    }
     if ($("#fail10").text() > "9") {
         $(".wrapper").css("background-color", "yellow");
-    };
+    }
     if ($("#fail11").text() > "9") {
         $(".wrapper").css("background-color", "yellow");
-    };
+    }
     if ($("#fail12").text() > "9") {
         $(".wrapper").css("background-color", "yellow");
-    };
+    }
 });
