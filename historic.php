6bfcd17964ade44ec8a5abbff80c934b05a06945
davidwintour <david.wintour@gmail.com>
2015-03-30 17:42:57 +0100

    css change

---
+++ historic.php
@@ -0,0 +1,89 @@
+<?php require("./head_foot/header_1.inc.php"); ?>
+
+<script type="text/javascript">
+   <?php echo 'FusionCharts.ready(function(){
+        var revenueChart = new FusionCharts({
+            type: "column2d",
+            renderAt: "chartContainer",
+            width: "1200",
+            height: "300",
+            dataFormat: "json",
+            dataSource: {
+                "chart": {
+                    "caption": "ESC Transactions Last Hour",
+                    "subCaption": "",
+                    "xAxisName": "Time",
+                    "yAxisName": "Amount",
+                    "canvasBgAlpha": "0",
+                    "logoURL": "/images/Vitruvian_Man_Outline_50x50.png",
+                  "logoAlpha": "40",
+                  "logoScale": "110",
+                  "logoPosition": "TR",
+                    "bgColor": "EEEEEE,CCCCCC",
+                    "bgratio": "60,40",
+                    "bgAlpha": "70,80",
+                    "theme": "ocean"
+                },
+                "data": [
+                    {
+                        "label": "-2 Days",
+                        "value": "'.$c1_amount3.'"
+                    },
+                    {
+                        "label": "-1 Days",
+                        "value": "'.$c1_amount2.'"
+                    },
+                    {
+                        "label": "Past 24 hours",
+                        "value": "'.$c1_amount1.'"
+                    }
+                   ],
+   "trendlines": [
+      {
+         "line": [
+            {
+               "startvalue": "0",
+               "color": "#1aaf5d",
+               "thickness": "2",
+               "valueOnRight": "1",
+               "tooltext": "Average Last 3 days",
+               "displayvalue": "Average"
+            }
+         ]
+      }
+   ]
+             }
+
+         });
+         revenueChart.render("chartContainer");
+     });'
+ ?>
+
+</script>
+
+
+<div class="wrapper">
+<br><BR>
+    <form method="post" action="<?php echo $PHP_SELF;?>">
+        <select name="opt">
+            <option value="1">Billed Successfully</option>
+            <option value="2">Submitted Charge, No Response</option>
+            <option value="3">Unkown Error</option>
+            <option value="4">Processing Payment, Pending</option>
+            <option value="5">Failed At Network</option>
+            <option value="600">Network Error</option>
+        </select>
+        <input type=text name=id>
+        <input type=submit value="Go">
+    </form>
+
+    <?
+    $serv=$_POST['serv'];
+    $id=$_POST['id'];
+    echo 'You selected Service '.$serv.' with ID '.$id;
+    ?>
+
+
+</div>
+
+<?php require("./head_foot/footer_1.inc.php"); ?>
---
+++ php/hist.php
@@ -0,0 +1,59 @@
+<?php
+$username = "pfimonuser";
+$password = "Fa6rUCha";
+$hostname = "localhost";
+
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password)
+or die("Unable to connect to MySQL");
+//echo "Connected to MySQL<br>";
+
+mysql_select_db("PFI_MON") or die(mysql_error());
+$r1 = array();
+// chart1
+$result1 = mysql_query("SELECT 	SUM(amount) as amount FROM `PFI_MON`.`overview_count` WHERE STATUS = '6' AND TIME >= SYSDATE() - INTERVAL 1 DAY;")
+or die(mysql_error());
+
+// store the record of the "example" table into $row
+while ($row = mysql_fetch_assoc($result1)) {
+    $r1[] = $row;
+}
+$c1_amount1 = $r1[0]['amount'];
+
+
+
+
+
+// chart2
+$r2 = array();
+$result2 = mysql_query("SELECT 	SUM(amount) AS amount FROM `PFI_MON`.`overview_count` WHERE STATUS = '6' AND TIME >= SYSDATE() - INTERVAL 2 DAY AND TIME <= SYSDATE() - INTERVAL 1 DAY;")
+or die(mysql_error());
+
+// store the record of the "example" table into $row
+while ($row2 = mysql_fetch_assoc($result2)) {
+    $r2[] = $row2;
+}
+$c2_amount1 = $r2[0]['amount'];
+
+
+
+
+// chart3
+$r3 = array();
+$result3 = mysql_query("SELECT 	SUM(amount) AS amount FROM `PFI_MON`.`overview_count` WHERE STATUS = '6' AND TIME >= SYSDATE() - INTERVAL 3 DAY AND TIME <= SYSDATE() - INTERVAL 2 DAY;")
+or die(mysql_error());
+
+// store the record of the "example" table into $row
+while ($row3 = mysql_fetch_assoc($result3)) {
+    $r3[] = $row3;
+}
+$c3_amount1 = $r3[0]['amount'];
+
+
+
+
+
+mysql_close($dbhandle);
+?>
+
+
