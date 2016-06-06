242bf1e551b3471387b4a419dace307b807d7d38
davidwintour <david.wintour@gmail.com>
2016-06-06 11:26:29 +0100

    css change

--- emailt2.php
+++ emailt2.php
@@ -1,31 +1,39 @@
 <?php
 
-require("./PHPMailerAutoload.php");
-//require("emailbody.php");
-
-$body1 = "test";
+require("./alerting/phpmailer/PHPMailerAutoload.php");
 
 function sendemail($body) {
 
     $mail = new PHPMailer;
 
+    //WINPLC MAIL
+    //mail.from=ngmp-report@winplc.com
+    //mail.username=ngmp-report
+    //mail.password=j6Zub4as
+    // mail.host=192.168.60.103
+
+    //HYDB MAIL
+    //mail.from=info@imimobile.com
+    //mail.host ="203.199.178.220"
+    //mail.auth = true
+    //mail.user ="alerts"
+    //mail.password ="alert@123"
+
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = '203.199.178.220';  // Specify main and backup SMTP servers // 220
+    $mail->Host = '192.168.60.103';  // Specify main and backup SMTP servers // 220
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'alerts';                 // SMTP username
-    $mail->Password = 'alert@123';                           // SMTP password
+    $mail->Username = 'ngmp-report';                 // SMTP username
+    $mail->Password = 'j6Zub4as';                           // SMTP password
 //$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 //$mail->Port = 587;                                    // TCP port to connect to
-    $mail->From = 'alerts@imimobile.com';
-    // $mail->FromName = 'PFI Alert';
+    $mail->From = 'report@winplc.com';
+    $mail->FromName = 'PFI Alert';
     $mail->addAddress('david.wintour@imimobile.com', 'Foobar');     // Add a recipient
-    // $mail->addAddress('operations@imimobile.com');             // Name is optional
-    // $mail->addAddress('tsg@imimobile.com');
     //$mail->addAddress('TSG@imimobile.com');               // Name is optional
 // $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('david.wintour@imimobile.com');
+    $mail->addCC('davidwintour@winplc.com');
 //$mail->addBCC('bcc@example.com');
 
 //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
@@ -34,7 +42,7 @@ function sendemail($body) {
 
 
 
-    $mail->Subject = 'test NW';
+    $mail->Subject = 'test email php';
     $mail->Body    = $body;
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
@@ -50,7 +58,10 @@ function sendemail($body) {
     }};
 
 
+$email1 = "email tester 123";
 
-    sendemail($body1);
+
+sendemail($email1);
+
 
 ?>
